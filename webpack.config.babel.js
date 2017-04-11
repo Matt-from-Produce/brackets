e212bd039c1e7e91c56cba712a29e0580a8cb4dc
Matthew trager <mtrager-home@Matthews-Air.home>
2017-04-11 15:43:42 -0400

    added rimraf, prod and dev builds

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -5,12 +5,15 @@
 // put my fucking project together because i have no idea how to build it without you!
 const { resolve } = require('path')
 
-module.exports = () => {
+module.exports = env => {
   return {
     context: resolve('src'),
     entry: './ClientApp.js',
     output: {
-      filename: './dist/bundle.js'
-    }
+      path: resolve('dist'),
+      filename: './bundle.js',
+      publicPath: '/dist/'
+    },
+    devtool : env.prod ? 'source-map' : 'eval'
   }
 }
