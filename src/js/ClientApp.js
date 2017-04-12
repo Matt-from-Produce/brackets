83c1d6393693086483302c08f245638082af2930
Matthew trager <mtrager-home@Matthews-Air.home>
2017-04-12 16:37:26 -0400

    webpack is now building react components

--- src/js/ClientApp.js
+++ src/js/ClientApp.js
@@ -1,17 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import MyTitle from './MyTitle'
+
 // first react component
 var div = React.DOM.div
-var h1 = React.DOM.h1
-
-// blueprint
-var MyTitle = React.createClass({
-  render: function () {
-    return (
-      div(null,
-        h1({ style: { color: this.props.color } }, this.props.title)
-      )
-    )
-  }
-})
 
 // convenience method not really necessary so we'll see
 var MyTitleFactory = React.createFactory(MyTitle)
--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,24 +1,22 @@
 
-
 // hey webpack
-
 
 const { resolve } = require('path')
 
 module.exports = env => {
   return {
     context: resolve('src'),
-    entry: './ClientApp.js',
+    entry: './js/ClientApp.js',
     output: {
       path: resolve('dist'),
       filename: './bundle.js',
       publicPath: '/dist/'
     },
-    devtool : env.prod ? 'source-map' : 'eval',
+    devtool: env.prod ? 'source-map' : 'eval',
     module: {
       loaders: [
          // transpile all js (except node packages), uses regex
-        {test: /\.js$/, loaders: ['babel-loader'], exclude: /node_modules/},
+        {test: /\.js$/, loaders: ['babel-loader'], exclude: /node_modules/}
         // TODO css loaded in js first via css-loader, then that js injected into dom at runtime with style-loader (injects style tags)
         // {test: /\.css$/, loaders: ['style-loader', 'css-loader']}
       ]
