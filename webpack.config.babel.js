990cd1dfbd9812a57034405d62c46284ac6ed82a
Matthew trager <mtrager-home@Matthews-Air.home>
2017-04-13 02:17:23 -0400

    removed all linting global installs and switched to local, add linting before building

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -23,7 +23,7 @@ module.exports = env => {
     devtool: env.prod ? 'source-map' : 'eval',
     module: {
       loaders: [
-         // transpile all js (except node packages), uses regex
+        {enforce: 'pre', test: /\.js$/, loaders: ['eslint-loader'], exclude: /node_modules/},
         {test: /\.js$/, loaders: ['babel-loader'], exclude: /node_modules/}
         // TODO css loaded in js first via css-loader, then that js injected into dom at runtime with style-loader (injects style tags)
         // {test: /\.css$/, loaders: ['style-loader', 'css-loader']}
