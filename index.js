97b52e36dde395905f50fbce209d7aa527e81b9c
Matthew trager <mtrager-home@Matthews-MacBook-Air.local>
2017-04-14 11:29:51 -0400

    connected to DB successfully!

--- index.js
+++ index.js
@@ -13,6 +13,23 @@ var logger = require('./server/utils/logger')
 // config.js is the FIRST thing that ever happens when starting the server
 // i feel like i already knew that but its good to remember that
 
+// try the connect here since i have a hold of config
+var mongoose = require('mongoose')
+var uri = config.uri
+var options = {
+  user: config.user,
+  pass: config.pass
+}
+mongoose.connect(uri, options).then(
+  function() {
+    logger.log('connected to mongoose')
+  },
+  function(err) {
+    logger.log('error connecting to mongoose')
+  }
+)
+var db = mongoose.connection
+
 app.listen(config.port, function() {
   logger.log('listen! http://localhost:' + config.port)
 })
--- server/server.js
+++ server/server.js
@@ -15,11 +15,15 @@ require('./middleware/appMiddleware')(app)
 // get the api
 app.use('/api', api)
 
-// connect
-
 // TODO somehow extrapolate this elsewhere? we should only get the app if we want it?
 // because this is specifically the web app?
 // serve from brackets folder and it will default to index on get '/'
+
+// NOTE
+// not really, because this is an SPA, so technically this is the correct way
+// well, it may not be the correct way but this is certainly the easiest way
+
+
 app.use(express.static('brackets'))
 
 // send index.html on GET request to '/'
