c197668f52944e5018094fd70e54b33baa2e8431
Matthew trager <mtrager-home@Matthews-Air.home>
2017-04-13 20:19:15 -0400

    i added react-router incorrectly somehow, it wouldnt render

--- src/js/ClientApp.js
+++ src/js/ClientApp.js
@@ -1,18 +1,16 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { BrowserRouter, Match } from 'react-router'
+// import { BrowserRouter, Match } from 'react-router'
 import Landing from './Landing'
 import Profile from './Profile'
 
 const App = React.createClass({
   render () {
     return (
-      <BrowserRouter>
-        <div className='app'>
-          <Match exactly pattern='/' component={Landing} />
-          <Match pattern='/profile' component={Profile} />
-        </div>
-      </BrowserRouter>
+      <div className='app'>
+        <Landing />
+        <Profile />
+      </div>
     )
   }
 })
