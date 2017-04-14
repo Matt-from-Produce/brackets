ec80594442124c646d1513b65d595f019087479f
Matthew trager <mtrager-home@Matthews-Air.home>
2017-04-14 07:23:08 -0400

    serve the app again :)

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 // server entry point
 
+// require is sync function
+// this builds the config object and does everything in there before continuing
 var config = require('./server/config/config')
+
+// same here
 var app = require('./server/server')
+
+// and here
 var logger = require('./server/utils/logger')
+
+// i feel like i already knew that but its good to remember that
+// config.js is the FIRST thing that ever happens when starting the server
 
 app.listen(config.port, function() {
   logger.log('listen! http://localhost:' + config.port)
--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ var api = require('./api/api')
 // TODO setup global error handling
 
 // var bodyParser = require('body-parser') // 3rd party middlware!
-// var path = require('path') // built-in middleware! (comes w/ node)
+var path = require('path') // built-in middleware! (comes w/ node)
 // var morgan = require('morgan') // 3rd party middleware!
 // var _ = require('lodash') // 3rd party middleware!
 // var PORT = 3000 // the port we will host on
@@ -14,50 +14,37 @@ var app = express()
 // setup the app middleware
 require('./middleware/appMiddleware')(app)
 
+// get the api
 app.use('/api', api)
 
+// serve from brackets folder and it will default to index on get '/'
+app.use(express.static('brackets'))
 
-// // serve from brackets folder and it will default to index on get '/'
-// app.use(express.static('brackets'))
-//
-// // use morgan: uses dev by default
-// if (process.env.NODE_ENV === 'prod') {
-//   app.use(morgan('tiny'))
-// } else {
-//   app.use(morgan('dev'))
-// }
-//
-// // bodyParser helps us send and recieve json data
-// app.use(bodyParser.urlencoded({extended: true}))
-// app.use(bodyParser.json())
+// send index.html on GET request to '/'
+app.get('/', function(req, res) {
+  res.sendFile(path.resolve('index.html'), function(err) {
+    if (err) {
+      res.status(500).send(err)
+    }
+  })
+})
 
-// // send index.html on GET request to '/'
-// app.get('/', function(req, res) {
-//   res.sendFile(path.resolve('index.html'), function(err) {
-//     if (err) {
-//       res.status(500).send(err)
-//     }
-//   })
-// })
-//
-// // give the bundle when its requested
-// app.get('/dist/bundle.js', function(req, res) {
-//   res.sendFile(path.resolve('dist/bundle.js'), function(err) {
-//     if (err) {
-//       res.status(500).send(err)
-//     }
-//   })
-// })
-//
-// // give the source-map when its requested
-// app.get('/dist/bundle.js.map', function(req, res) {
-//   res.sendFile(path.resolve('dist/bundle.js.map'), function(err) {
-//     if (err) {
-//       res.status(500).send(err)
-//     }
-//   })
-// })
-//
-// // export
+// give the bundle when its requested
+app.get('/dist/bundle.js', function(req, res) {
+  res.sendFile(path.resolve('dist/bundle.js'), function(err) {
+    if (err) {
+      res.status(500).send(err)
+    }
+  })
+})
+
+// give the source-map when its requested
+app.get('/dist/bundle.js.map', function(req, res) {
+  res.sendFile(path.resolve('dist/bundle.js.map'), function(err) {
+    if (err) {
+      res.status(500).send(err)
+    }
+  })
+})
 
 module.exports = app
