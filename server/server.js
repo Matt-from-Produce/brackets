d0f6f8c15c0c11324a96369773f7a3e4d96059c7
Matthew trager <mtrager-home@Matthews-Air.home>
2017-06-21 10:32:20 -0400

    correctly serve static assets via express static method

--- server/server.js
+++ server/server.js
@@ -33,23 +33,7 @@ app.use('/api', api)
 app.use('/auth', auth)
 
 // serve static assets
-app.use(express.static('brackets'))
-//
-// app.use((req, res) => {
-//   var context = {}
-//   var body = ReactDOMServer.renderToString(
-//     React.createElement(StaticRouter, { location: req.url, context },
-//       React.createElement(App)
-//     )
-//   )
-//
-//   if (context.url) {
-//     // TODO if there is a redirect
-//   }
-//
-//   res.write(template({body: body}))
-//   res.end()
-// })
+app.use('/dist', express.static('./dist'))
 
 // send index.html on GET request to '/'
 app.get('/', function(req, res) {
@@ -66,33 +50,6 @@ app.get('/', function(req, res) {
 
   res.write(template({body: body}))
   res.end()
-})
-
-// // send index.html on GET request to '/'
-// app.get('/', function(req, res) {
-//   res.sendFile(path.resolve('index.html'), function(err) {
-//     if (err) {
-//       res.status(500).send(err)
-//     }
-//   })
-// })
-
-// give the bundle when its requested
-app.get('/dist/bundle.js', function(req, res) {
-  res.sendFile(path.resolve('dist/bundle.js'), function(err) {
-    if (err) {
-      res.status(500).send(err)
-    }
-  })
-})
-
-// give the source-map when its requested
-app.get('/dist/bundle.js.map', function(req, res) {
-  res.sendFile(path.resolve('dist/bundle.js.map'), function(err) {
-    if (err) {
-      res.status(500).send(err)
-    }
-  })
 })
 
 module.exports = app
