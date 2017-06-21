a07f9c1ef84d573ca98d780e6ff1fc9df2271378
Matthew trager <mtrager-home@Matthews-Air.home>
2017-06-20 22:36:17 -0400

    added styled-components

--- src/js/App.js
+++ src/js/App.js
@@ -2,12 +2,18 @@ import React from 'react'
 import { Route } from 'react-router-dom'
 import Landing from './Landing'
 import Main from './Main'
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+  margin: auto;
+  max-width: 640px;
+`
 
 const App = () => (
-  <div className='app'>
+  <Wrapper>
     <Route exact path='/' component={Landing} />
     <Route path='/main' component={Main} />
-  </div>
+  </Wrapper>
 )
 
 export default App
