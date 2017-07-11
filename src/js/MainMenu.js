c83c1e42701aa7e00d230cd573cf83daa61d391d
Matthew trager <mtrager-home@Matthews-Air.home>
2017-07-11 15:53:30 -0400

    added silly main menu component

---
+++ src/js/MainMenu.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+class MainMenu extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      bullshit: true
+    }
+  }
+
+  render () {
+    return (
+      <div>
+        <h3>this is the main menu lol</h3>
+      </div>
+    )
+  }
+}
+
+export default MainMenu
