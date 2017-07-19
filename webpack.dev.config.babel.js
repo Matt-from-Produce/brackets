52c3f8e985140e144d845a0402c2da1a8c93e776
Matthew trager <mtrager-home@Matthews-Air.home>
2017-07-19 12:29:30 -0400

    different webpack files for dev/prod, add json-loader

---
+++ webpack.dev.config.babel.js
@@ -0,0 +1,35 @@
+
+// hey webpack
+
+const { resolve } = require('path')
+
+module.exports = env => {
+  return {
+    context: resolve('src'),
+    entry: './js/ClientApp.js',
+    output: {
+      path: resolve('dist'),
+      filename: './bundle.js',
+      publicPath: '/dist/'
+    },
+    resolve: {
+      extensions: ['.js']
+    },
+    devServer: {
+      historyApiFallback: true
+    },
+    stats: {
+      colors: true,
+      reasons: true,
+      chunks: true
+    },
+    devtool: env.prod ? 'source-map' : 'eval',
+    module: {
+      loaders: [
+        {enforce: 'pre', test: /\.js$/, loaders: ['eslint-loader'], exclude: /node_modules/},
+        {test: /\.js$/, loaders: ['babel-loader'], exclude: /node_modules/},
+        {test: /\.json$/, loaders: ['json-loader'], exclude: /node_modules/}
+      ]
+    }
+  }
+}
---
+++ webpack.prod.config.babel.js
@@ -0,0 +1,35 @@
+
+// hey webpack
+
+const { resolve } = require('path')
+
+module.exports = () => {
+  return {
+    context: resolve('src'),
+    entry: './js/ClientApp.js',
+    output: {
+      path: resolve('dist'),
+      filename: './bundle.js',
+      publicPath: '/dist/'
+    },
+    resolve: {
+      extensions: ['.js']
+    },
+    devServer: {
+      historyApiFallback: true
+    },
+    stats: {
+      colors: true,
+      reasons: true,
+      chunks: true
+    },
+    devtool: 'source-map'
+    module: {
+      loaders: [
+        {enforce: 'pre', test: /\.js$/, loaders: ['eslint-loader'], exclude: /node_modules/},
+        {test: /\.js$/, loaders: ['babel-loader'], exclude: /node_modules/},
+        {test: /\.json$/, loaders: ['json-loader'], exclude: /node_modules/}
+      ]
+    }
+  }
+}
