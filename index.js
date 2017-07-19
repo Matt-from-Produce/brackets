e2c135e665a3e8b79bce0fae1316cb01b5128e1c
Matthew trager <mtrager-home@Matthews-Air.home>
2017-07-19 12:36:43 -0400

    start a json-server in dev

--- index.js
+++ index.js
@@ -10,15 +10,30 @@ var options = {
   pass: config.pass
 }
 
-// connect to db
-mongoose.connect(uri, options).then(
-  function() {
-    logger.log('connected to mongoose')
-  },
-  function(err) {
-    logger.log('error connecting to mongoose')
-  }
-)
+// set up mock db if we're in dev
+if (config.env === 'dev') {
+  console.log('hitting that shit')
+  var jsonServer = require('json-server')
+  var server = jsonServer.create()
+  var router = jsonServer.router('db.json')
+  var middlewares = jsonServer.defaults()
+
+  server.use(middlewares)
+  server.use(router)
+  server.listen(3001, () => {
+    console.log('JSON server listening on port 3001')
+  })
+} else {
+  // connect to db
+  mongoose.connect(uri, options).then(
+    function() {
+      logger.log('connected to mongoose')
+    },
+    function(err) {
+      logger.log('error connecting to mongoose')
+    }
+  )
+}
 
 app.listen(config.port, function() {
   logger.log('listen! http://localhost:' + config.port)
